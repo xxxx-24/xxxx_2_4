ce985474d5f2c08cfad0d45ca55ddbb3b3fa45d2
xxxx-24 <158561783+xxxx-24@users.noreply.github.com>
2024-02-12 19:44:19 +0800

    Add files via upload

---
+++ Project24_2_5c书/Project24_2_5c书/源.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//
+//int main()
+//{
+//	printf("\n");
+//	printf("       *\n");
+//	printf("      ***\n");
+//	printf("     *****\n");
+//	printf("    *******\n");
+//	printf("     *****\n");
+//	printf("      ***\n");
+//	printf("       *\n\n");
+//	system("pause");
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	printf("I---------I----------I\n");
+//	printf("I   中国  I   美国   I\n");
+//	printf("I_________I__________I\n");
+//	system("pause");
+//	return 0;
+//}
+
