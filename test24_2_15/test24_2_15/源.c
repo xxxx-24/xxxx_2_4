ab4aa17892692c93a71032c873e728a7f286b386
xxxx-24 <158561783+xxxx-24@users.noreply.github.com>
2024-02-29 16:02:17 +0800

    Add files via upload

---
+++ test24_2_15/test24_2_15/源.c
@@ -0,0 +1,50 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10, };
+//	char ch[5] = { 'a','b','c' };
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//int main()
+//{
+//	int x, d1, d2, d3, d4;
+//	printf("5389");     //输入一个四位数
+//	scanf("%d", &x);
+//	d1 = x % 10;        //个位
+//	d2 = x /10% 10;    //十位
+//	d3 = x/100 % 10;    //百位
+//	d4 = x/1000 % 10;    //千位
+//	printf("千位=%d，百位=%d,十位=%d,个位=%d\n", 
+//		d4, d3, d2, d1);
+//	return 0;
+//
+//
+//}
+
+//#include<stdio.h>
+//#include<stdlib.h>
+//int main()
+//{
+//	int age;
+//	float weight;
+//	char name[15];
+//	printf("\n请输入你的姓名；");//scanf前面用printf做提示
+//	scanf("%s", name);
+//	printf("请输入你的年龄；");
+//	scanf("%d", &age);
+//	printf("请输入你的体重(公斤）；");
+//	scanf("%f", &weight);
+//	printf("\n以下请输入个人信息\n");//运行之后用户操作输入数据运行下面的语句
+//	printf("姓名；%s\n", name);
+//	printf("年龄；%d\n", age);
+//	printf("体重；%.2f公斤\n", weight);
+//	system("pause");
+//	return 0;
+//	
+//}
+
+
